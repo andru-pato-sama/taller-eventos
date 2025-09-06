3be320692c4955980d21a7f85589e06668534c08
andrés-pato-sama <ramirezmarrocoandres@gmail.com>
2025-09-06 18:48:04 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const divo = document.getElementById('divo');
+const botoncito = document.getElementById('botoncito');
+
 
 divo.addEventListener("click", function (){
     alert('Hola! Soy el div');
 });
+
+botoncito.addEventListener("click", function(){
+    botoncito.stopPropagation();
+})
